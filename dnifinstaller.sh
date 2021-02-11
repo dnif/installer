9b13d2ab54c9e206a7abed0116d9c1a2b25e7405
rohantiwatne <rohan.tiwatne@net-mon.net>
2021-02-11 17:37:57 +0530

    update dnifinstaller

--- dnifinstaller.sh
+++ dnifinstaller.sh
@@ -5,28 +5,33 @@
 function docker_check() {
 	echo -e "[*]Installing docker \n"
 	sudo apt-get remove docker docker-engine docker.io containerd runc &>/dev/null
-	sudo apt-get install \
-            apt-transport-https \
-            ca-certificates \
-            curl \
-            gnupg-agent \
-            software-properties-common &>/dev/null
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>/dev/null 
-	sudo apt-key fingerprint 0EBFCD88 &>/dev/null 
-        sudo add-apt-repository \ 
-         "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-         $(lsb_release -cs) \
-         stable" &>/dev/null
 	sudo apt-get -y update &>/dev/null
-        sudo apt-get -y install docker-ce docker-ce-cli containerd.io &>/dev/null
-        sleep 5
+	sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common &>/dev/null
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>/dev/null
+	sudo apt-key fingerprint 0EBFCD88 &>/dev/null
+	sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &>/dev/null
+	sudo apt-get -y update &>/dev/null
+	echo -e "[*] Installing docker-ce\n"
+	sudo apt-get -y install docker-ce docker-ce-cli containerd.io &>/dev/null
+	sleep 5
         sudo docker run hello-world &>/dev/null
+	echo -e "[*] Hello from Docker\n"
         sleep 5
+	
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &>/dev/null
 	sudo chmod +x /usr/local/bin/docker-compose 
+	echo -e "[*] Installing docker-compose - DONE"
         count=$(sysctl -n vm.max_map_count)
 	if [ "$count" = "262144" ]; then
-		echo -e "max count set"
+		echo -e "[*] max count alrady set"
 		ufw reset &>/dev/null
 
 	else
@@ -53,38 +58,42 @@ ARCH=$(uname -m)
 VER=$(lsb_release -rs)
 
 
+echo -e "* DNIF Installer for v9.1beta2"
+echo -e "** Please report issues to https://github.com/dnif-backyard/installer/issues**\n"
+echo -e "[*] Checking operating system for compatibility...\n"
+
+
 if [[ "$VER" = "20.04" ]] && [[ "$ARCH" = "x86_64" ]];  then # replace 18.04 by the number of release you want
 
-       echo "Compatible version"
+       echo "[*] Compatible version"
        #Copy your files here
-       echo -e "* DNIF Installer for v9.1beta2"
-       echo -e "** Please report issues to https://github.com/dnif-backyard/installer/issues**\n"
-       echo -e "[*] Checking operating system for compatibility - DONE\n"
+       echo -e "[*] Checking operatingi system for compatibility - DONE\n"
        echo -e "* Select a DNIF component you would like to install\n"
        echo -e "** for more information visit https://docs.dnif.it/v91/docs/high-level-dnif-architecture\n"
        echo -e "[1]- Core (CO) \n"
        echo -e "[2]- Data Node (DN) \n"
        echo -e "[3]- Adapter (AD) \n"
        echo -e "[4]- Local Console (LC) \n"
-       echo -e "ENTER COMPONENT NAME:  \n "
+       echo -e "ENTER COMPONENT NAME:   "
        read -r COMP
+       echo -e "-----------------------------------------------------------------------------------------"
        case "${COMP^^}" in
 	       CO)
 		       echo -e "[*] Installing the CORE \n"
 		       sleep 2
 		       echo -e "[*] Finding docker installation\n"
 		       if [ -x "$(command -v docker)" ]; then
-			       echo "[*]Updating Docker"
+			       echo "[*] Updating Docker\n"
 			       docker_check
 			else
 				echo -e "[*] Finding docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker"
+				echo -e "[*] Installaing Docker\n"
 				docker_check
-				echo -e "[*]Finding docker installation - DONE"
-				echo -e "[*] Finding docker-compose - DONE"
+				echo -e "[*] Finding docker installation - DONE\n"
+				
 			fi
-			echo -e "[*] Pulling Docker Image for CORE"
-			#docker pull dnif/core:v9beta2.2
+			echo -e "[*] Pulling Docker Image for CORE\n"
+			docker pull dnif/core:v9beta2.2 &>/dev/null
 			cd /
 			sudo mkdir -p DNIF
 			echo -e "Enter CORE IP:\c"
@@ -110,24 +119,26 @@ services:
     container_name: core-v9" >/DNIF/docker-compose.yml
 
 			      cd /DNIF || exit
-			      #docker-compose up -d
+			      echo -e "[*] Starting container... \n"
+			      docker-compose up -d
+			      echo -e "[*] Starting container... DONE\n"
 			      ;;
 		AD)
 			echo -e "[*] Installing the ADAPTER \n"
 			sleep 5
 			echo -e "[*] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
-				echo "[*] Updating Docker"
+				echo "[*] Updating Docker\n"
 				docker_check
 			else
 				echo -e "[*] Finding docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker"
+				echo -e "[*] Installaing Docker\n"
 				docker_check
-				echo -e "[*]Finding docker installation - DONE"
-				echo -e "[*] Finding docker-compose - DONE"
+				echo -e "[*] Finding docker installation - DONE\n"
+				echo -e "[*] Finding docker-compose - DONE\n"
 			fi
-			echo -e "[*] Pulling Docker Image for Adapter"
-			#docker pull dnif/adapter:v9beta2.2
+			echo -e "[*] Pulling Docker Image for Adapter\n"
+			docker pull dnif/adapter:v9beta2.2 &>/dev/null
 			echo -e "ENTER CORE IP: \c"
 			read -r COREIP
 			cd /
@@ -148,7 +159,9 @@ services:
    - /backup:/backup
   container_name: adapter-v9" >/DNIF/AD/docker-compose.yaml
 			  cd /DNIF/AD || exit
-			  #docker-compose up -d
+			  echo -e "[*] Starting container...\n "
+			  docker-compose up -d
+			  echo -e "[*] Starting container... DONE\n"
 			  ;;
 
 		LC)
@@ -156,17 +169,17 @@ services:
 			sleep 5
 			echo -e "[*] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
-				echo "[*] Updating Docker"
+				echo "[*] Updating Docker\n"
 				docker_check
 			else
 				echo -e "[*] Finding docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker"
+				echo -e "[*] Installaing Docker\n"
 				docker_check
-				echo -e "[*]Finding docker installation - DONE"
-				echo -e "[*] Finding docker-compose - DONE"
+				echo -e "[*] Finding docker installation - DONE\n"
+				echo -e "[*] Finding docker-compose - DONE\n"
 			fi
-			#docker pull dnif/console:v9beta2.2
-			echo -e "[*]Pulling Docker Image for Local Console"
+			docker pull dnif/console:v9beta2.2 &>/dev/null
+			echo -e "[*] Pulling Docker Image for Local Console\n"
 			echo -e "ENTER INTERFACE NAME: \c"
 			read -r INTERFACE
 			cd /
@@ -186,21 +199,23 @@ services:
    - /dnif/LC:/dnif/lc
   container_name: console-v9" >/DNIF/LC/docker-compose.yaml
 			  cd /DNIF/LC || exit
-			  #docker-compose up -d
+			  echo -e "[*] Starting container... \n"
+			  docker-compose up -d
+			  echo -e "[*] Starting container... DONE\n"
 			  ;;
 		DN)
 			echo -e "[*] Installing the DATA NODE \n"
 			sleep 5
 			echo -e "[*] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
-				echo "[*] Updating Docker"
+				echo "[*] Updating Docker\n"
 				docker_check
 			else
 				echo -e "[*] Finding docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker"
+				echo -e "[*] Installaing Docker\n"
 				docker_check
-				echo -e "[*]Finding docker installation - DONE"
-				echo -e "[*] Finding docker-compose - DONE"
+				echo -e "[*] Finding docker installation - DONE\n"
+				echo -e "[*] Finding docker-compose - DONE\n"
 			fi
 			echo -e "[*] Checking for JDK \n"
 			if type -p java; then
@@ -210,12 +225,12 @@ services:
 				_java="$JAVA_HOME/bin/java"
 			else
 				echo -e "\n [*]To proceed futher you have to  Install openjdk14 before installtion\n\n"
-				echo "[*]To install OpenJdk14 type YES"
+				echo "[*] To install OpenJdk14 type YES"
 				read -r var
 				if [ "$var" == "YES" ]; then
-					apt-get -y install openjdk-14-jdk
+					apt-get -y install openjdk-14-jdk &>/dev/null
 				else
-					echo "[*]Aborted"
+					echo "[*] Aborted"
 					exit 0
 				fi
 			fi
@@ -226,8 +241,8 @@ services:
 				fi
 			fi
 			sleep 5
-			echo -e "[*]Pulling Docker Image for Data Node"
-			#docker pull dnif/datanode:v9beta2.2
+			echo -e "[*] Pulling Docker Image for Data Node\n"
+			docker pull dnif/datanode:v9beta2.2
 			echo -e "ENTER CORE IP: \c\n"
 			read -r COREIP
 			echo -e "\nENter INTERFACE NAME"
@@ -259,7 +274,9 @@ services:
          hard: -1
     container_name: datanode-v9" >/DNIF/DL/docker-compose.yaml
 			    cd /DNIF/DL || exit
-			    #docker-compose up -d
+			    echo -e "[*] Starting container... \n"
+			    docker-compose up -d
+			    echo -e "[*] Starting container... DONE"
 			    ;;
 		esac
 
