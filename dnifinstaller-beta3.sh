0f5ceb7e805015779e5781785be65546b35fb117
rohantiwatne9 <rohan.tiwatne@net-mon.net>
2021-02-19 15:13:00 +0530

    update dnifinstaller

--- dnifinstaller-beta3.sh
+++ dnifinstaller-beta3.sh
@@ -6,10 +6,10 @@ function docker_check() {
 
 	version=$(docker --version |cut -c 16-22)
 	if [[ "$version" != "20.10.3" ]]; then
-		echo -e "[-] Finding docker installation - found incompatible version\n"
-		echo -e "Found incompatible version ... \e[0;31m[ERROR] \e[0m\n"
-		#echo -e "[*] Updating Docker\n"
-		echo -e "[-] Uninstalling Docker\n"
+		echo -n "[-] Finding docker installation - found incompatible version"
+		echo -e "... \e[0;31m[ERROR] \e[0m\n"
+		#echo -e "[*] Updating docker\n"
+		echo -e "[-] Uninstalling docker\n"
 		sudo apt-get remove docker docker-engine docker.io containerd runc
 		sudo apt-get -y update
 		echo -e "[-] Setting up docker-ce respositories\n"
@@ -26,23 +26,24 @@ function docker_check() {
    $(lsb_release -cs) \
    stable"
 		sudo apt-get update
-		echo -e "[-] Installing Docker-ce\n"
+		echo -e "[-] Installing docker-ce\n"
 		sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 	fi
-	echo -e "[-] Finding Docker installation - DONE\n"
+	echo -n "[-] Finding docker installation "
+	echo -e " ... \e[1;32m[DONE] \e[0m\n"
 	version=$(docker-compose --version |cut -c 24-29)
 	if [[ "$version" != "1.23.1" ]]; then
-		echo -e "[*] Installing Docker-compose\n"
+		echo -e "[-] Installing docker-compose\n"
 		sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 		sudo chmod +x /usr/local/bin/docker-compose 
-		echo -e "[*] Installing Docker-compose - DONE\n"
+		echo -e "[-] Installing docker-compose - ... \e[1;32m[DONE] \e[0m\n"
 	else
-		echo -e "[*] Docker-compose up-to-date\n"
+		echo -e "[-] docker-compose up-to-date\n"
 	fi
         count=$(sysctl -n vm.max_map_count)
 	if [ "$count" = "262144" ]; then
-		echo -e "[*] Fine tuning the operating system\n"
+		echo -e "[-] Fine tuning the operating system\n"
 		ufw reset 
 
 	else
@@ -69,72 +70,75 @@ ARCH=$(uname -m)
 VER=$(lsb_release -rs)
 
 
-echo -e "\n\nDNIF Installer for v9.1beta3\n"
-echo -e "for more information and code visit https://github.com/dnif-backyard/installer\n\n"
+echo -e "\nDNIF Installer for v9.1beta3\n"
+echo -e "for more information and code visit https://github.com/dnif-backyard/installer\n"
 
 echo -e "++ Checking operating system for compatibility...\n"
 
-
+echo -n "Operating system compatibility"
+sleep 2
 if [[ "$VER" = "20.04" ]] && [[ "$ARCH" = "x86_64" ]];  then # replace 20.04 by the number of release you want
 
-       echo -e "Operating system compatibility ... [OK] \n"
+       #echo -n "Operating system compatibility"
+       echo -e " ... \e[1;32m[OK] \e[0m\n"
        #Copy your files here
-       echo -e "Architecture compatibility ... [OK] \n"
+       echo -n "Architecture compatibility "
+       echo -e " ... \e[1;32m[OK] \e[0m\n"
        echo -e "** found Ubuntu 20.04 (LTS) x86_64\n\n"
-       echo -e "[*] Checking operating system for compatibility - DONE\n\n"
+       echo -e "[-] Checking operating system for compatibility - ... \e[1;32m[DONE] \e[0m\n"
        echo -e "** Please report issues to https://github.com/dnif-backyard/installer/issues\n"
        echo -e "* Select a DNIF component you would like to install\n"
        echo -e "** for more information visit https://docs.dnif.it/v91/docs/high-level-dnif-architecture\n"
-       echo -e "[1]- Core (CO) AND Data Node Master\n"
-       echo -e "[2]- Console (LC) \n"
-       echo -e "[3]- Data Node (DN) \n"
-       echo -e "[4]- Adapter (AD)\n"
+       echo -e "    [1] Core (CO) AND Data Node Master\n"
+       echo -e "    [2] Console (LC) \n"
+       echo -e "    [3] Data Node (DN) \n"
+       echo -e "    [4] Adapter (AD)\n"
        read -p "Pick the number corresponding to the component (1 - 4): " COMP
        #read -r COMP
        echo -e "-----------------------------------------------------------------------------------------"
        case "${COMP^^}" in
 	       1)
-		       echo -e "[*] Installing the CORE AND Data Node Master \n"
+		       echo -e "[-] Installing the CORE AND Data Node Master \n"
 		       sleep 2
-		       echo -e "[*] Finding Docker installation\n"
+		       echo -e "[-] Finding docker installation\n"
 		       if [ -x "$(command -v docker)" ]; then
 			       #echo -e "[*] Updating Docker\n"
 			       docker_check
 			else
-				echo -e "[*] Finding Docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker\n"
+				echo -e "[-] Finding docker installation - ... \e[1;31m[NEGATIVE] \e[0m\n"
+				echo -e "[-] Installaing docker\n"
 				docker_check
-				echo -e "[*] Finding Docker installation - DONE\n"
+				echo -e "[-] Finding docker installation - ... \e[1;32m[DONE] \e[0m\n"
 				
 			fi
-			echo -e "[*] Checking for JDK \n"
+			echo -e "[-] Checking for JDK \n"
 			if type -p java; then
 				_java=java
 			elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
 				echo -e "\nfound java executable in $JAVA_HOME \n"
 				_java="$JAVA_HOME/bin/java"
 			else
-				echo -e "[*] To proceed futher you have to  Install openjdk14 before installtion\n"
-				echo "[*] To install OpenJdk14 type YES or NO"
+				echo -e "[-] To proceed futher you have to  Install openjdk14 before installtion\n"
+				echo "[-] To install OpenJdk14 type YES or NO"
 				read -r var
 				temp=${var^^}
 				if [ "$temp" == "YES" ]; then
 					apt-get -y install openjdk-14-jdk 
 				else
-					echo "[*] Aborted"
+					echo "[-] Aborted"
 					exit 0
 				fi
 			fi
 			if [[ "$_java" ]]; then
 				version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 				if [[ "$version" == "14.0.2" ]]; then
-					echo -e "[*] OpenJdk $version version is running\n"
+					echo -e "[-] OpenJdk $version version is running\n"
 				fi
 			fi
-			echo -e "[*] Pulling Docker Image for CORE\n"
-			docker pull dnif/core:v9beta3		# replace tag by the number of release you want
-			echo -e "[*] Pulling Docker Image for Data Node\n"
-			docker pull dnif/datanode:v9beta3		# replace tag by the number of release you want
+			echo -e "[-] Pulling docker Image for CORE\n"
+			docker pull dnif/core:v9beta3 
+			echo -e "[-] Pulling docker Image for Data Node\n"
+			docker pull dnif/datanode:v9beta3    # replace tag by the number of release you want
 			cd /
 			sudo mkdir -p DNIF
 			COREIP=""
@@ -184,30 +188,30 @@ services:
     container_name: datanode-master-v9" >/DNIF/docker-compose.yaml
 
 			      cd /DNIF || exit
-			      echo -e "[*] Starting container... \n"
+			      echo -e "[-] Starting container... \n"
 			      docker-compose up -d
-			      echo -e "[*] Starting container... DONE\n"
+			      echo -e "[-] Starting container... \e[1;32m[DONE] \e[0m\n"
 			      docker ps
 			      echo -e "** Congratulations you have successfully installed the CORE AND Data Node Master\n"
 			      ;;
 			    
 
 		2)
-			echo -e "[*] Installing the Console \n"
+			echo -e "[-] Installing the Console \n"
 			sleep 5
-			echo -e "[*] Finding Docker installation\n"
+			echo -e "[-] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
 				#echo "[*] Updating Docker\n"
 				docker_check
 			else
-				echo -e "[*] Finding Docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker\n"
+				echo -e "[-] Finding docker installation - ... \e[1;31m[NEGATIVE] \e[0m\n"
+				echo -e "[-] Installaing docker\n"
 				docker_check
-				echo -e "[*] Finding Docker installation - DONE\n"
-				echo -e "[*] Finding Docker-compose - DONE\n"
+				echo -e "[-] Finding docker installation - ... \e[1;32m[DONE] \e[0m\n"
+				echo -e "[-] Finding docker-compose - ... \e[1;32m[DONE] \e[0m\n"
 			fi
-			echo -e "[*] Pulling Docker Image for Console\n"
-			docker pull dnif/console:v9beta3 		# replace tag by the number of release you want
+			echo -e "[-] Pulling docker Image for Console\n"
+			docker pull dnif/console:v9beta3   	# replace tag by the number of release you want
 			cd /
 			sudo mkdir -p /DNIF
 			sudo mkdir -p /DNIF/LC
@@ -223,52 +227,52 @@ services:
    - /dnif/LC:/dnif/lc
   container_name: console-v9" >/DNIF/LC/docker-compose.yaml
 			  cd /DNIF/LC || exit
-			  echo -e "[*] Starting container... \n"
+			  echo -e "[-] Starting container... \n"
 			  docker-compose up -d
-			  echo -e "[*] Starting container... DONE\n"
+			  echo -e "[-] Starting container... DONE\n"
 			  docker ps
 			  echo -e "** Congratulations you have successfully installed the Console\n"
 			  ;;
 		3)
-			echo -e "[*] Installing the DATA NODE \n"
+			echo -e "[-] Installing the DATA NODE \n"
 			sleep 5
-			echo -e "[*] Finding Docker installation\n"
+			echo -e "[-] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
 				#echo -e "[*] Updating Docker\n"
 				docker_check
 			else
-				echo -e "[*] Finding Docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker\n"
+				echo -e "[-] Finding docker installation - ... \e[1;31m[NEGATIVE] \e[0m\n"
+				echo -e "[-] Installaing docker\n"
 				docker_check
-				echo -e "[*] Finding Docker installation - DONE\n"
-				echo -e "[*] Finding Docker-compose - DONE\n"
+				echo -e "[-] Finding docker installation - ... \e[1;32m[DONE] \e[0m\n"
+				echo -e "[-] Finding docker-compose - ... \e[1;32m[DONE] \e[0m\n"
 			fi
-			echo -e "[*] Checking for JDK \n"
+			echo -e "[-] Checking for JDK \n"
 			if type -p java; then
 				_java=java
 			elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-				echo -e "\nfound java executable in $JAVA_HOME \n"
+				echo -e "\n\nfound java executable in $JAVA_HOME \n\n"
 				_java="$JAVA_HOME/bin/java"
 			else
-				echo -e "[*] To proceed futher you have to  Install openjdk14 before installtion\n"
-				echo "[*] To install OpenJdk14 type YES or NO"
+				echo -e "[-] To proceed futher you have to  Install openjdk14 before installtion\n"
+				echo "[-] To install OpenJdk14 type YES or NO"
 				read -r var
 				temp=${var^^}
 				if [ "$temp" == "YES" ]; then
 					apt-get -y install openjdk-14-jdk 
 				else
-					echo "[*] Aborted"
+					echo "[-] Aborted"
 					exit 0
 				fi
 			fi
 			if [[ "$_java" ]]; then
 				version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 				if [[ "$version" == "14.0.2" ]]; then
-					echo -e "[*] OpenJdk $version version is running\n"
+					echo -e "[-] OpenJdk $version version is running\n"
 				fi
 			fi
 			sleep 5
-			echo -e "[*] Pulling Docker Image for Data Node\n"
+			echo -e "[-] Pulling docker Image for Data Node\n"
 			docker pull dnif/datanode:v9beta3		# replace tag by the number of release you want
 			COREIP=""
 			while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
@@ -301,9 +305,9 @@ services:
         hard: -1
     container_name: datanode-v9" >/DNIF/DL/docker-compose.yaml
 			    cd /DNIF/DL || exit
-			    echo -e "[*] Starting container... \n"
+			    echo -e "[-] Starting container... \n"
 			    docker-compose up -d
-			    echo -e "[*] Starting container... DONE"
+			    echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m"
 			    docker ps
 			    echo -e "** Congratulations you have successfully installed the Data Node\n"
 			    echo -e "**   Active the Data Node (10.2.1.4) from the components page\n"
@@ -311,22 +315,22 @@ services:
 			    
 
 		4)
-			echo -e "[*] Installing the ADAPTER \n"
+			echo -e "[-] Installing the ADAPTER \n"
 			sleep 5
-			echo -e "[*] Finding Docker installation\n"
+			echo -e "[-] Finding docker installation\n"
 			if [ -x "$(command -v docker)" ]; then
 				#echo -e "[*] Updating Docker\n"
 				docker_check
 			else
-				echo -e "[*] Finding Docker installation - NEGATIVE\n"
-				echo -e "[*] Installaing Docker\n"
+				echo -e "[-] Finding docker installation  ... \e[1;31m[NEGATIVE] \e[0m\n"
+				echo -e "[-] Installaing docker\n"
 				docker_check
-				echo -e "[*] Finding Docker installation - DONE\n"
-				echo -e "[*] Finding Docker-compose - DONE\n"
+				echo -e "[-] Finding docker installation ... \e[1;32m[DONE] \e[0m\n"
+				echo -e "[-] Finding docker-compose ... \e[1;32m[DONE] \e[0m\n"
 				
 			fi
-			echo -e "[*] Pulling Docker Image for Adapter\n"
-			docker pull dnif/adapter:v9beta3		# replace tag by the number of release you want
+			echo -e "[-] Pulling docker Image for Adapter\n"
+			docker pull dnif/adapter:v9beta3 		# replace tag by the number of release you want
 			#echo -e "ENTER CORE IP: \c"
 			COREIP=""
 			while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
@@ -351,9 +355,9 @@ services:
    - /backup:/backup
   container_name: adapter-v9" >/DNIF/AD/docker-compose.yaml
 			  cd /DNIF/AD || exit
-			  echo -e "[*] Starting container...\n "
+			  echo -e "[-] Starting container...\n "
 			  docker-compose up -d
-			  echo -e "[*] Starting container... DONE\n"
+			  echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m\n"
 			  docker ps
 			  echo -e "** Congratulations you have successfully installed the Adapter\n"
 			  echo -e "**   Active the Adapter (10.2.1.4) from the components page\n"
@@ -366,6 +370,6 @@ services:
 
 
 else
-       echo -e  "\e[0;31m[ERROR] \e[0m Operating system is incompatible"
+       echo -e "\e[0;31m[ERROR] \e[0m Operating system is incompatible"
 fi
 
