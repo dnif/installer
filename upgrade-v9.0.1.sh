8c84fea489029c24d512759473477580efbb87c1
rohantiwatne9 <rohan.tiwatne@net-mon.net>
2021-03-31 18:43:35 +0530

    upload license_path_change file

--- upgrade-v9.0.1.sh
+++ upgrade-v9.0.1.sh
@@ -40,33 +40,36 @@ upgrade_docker_container () {
 
 
 
+if [[ $EUID -ne 0 ]]; then
+    echo -e "This script must be run as root ... \e[1;31m[ERROR] \e[0m\n"
+    exit 1
+else
+	container_list=("core" "datanode" "adapter" "console" )
+	echo -e "[-] Finding docker Image"
+	for i in "${container_list[@]}"
+	do
+		#echo -e "[-] Finding docker Image"
+		if [ "$(docker images|grep $i|awk 'NR==1 {print $1; exit}'|cut -d "/" -f2)" ]; then
+			#echo -e "[-] Found $i docker container"
+			echo -e "[-] Checking for current running version"
+			sleep 3
+			current_tag="$(docker images|grep $i|awk 'NR==1 {print $2; exit}')"
+			
+			echo -e "[-] Found current version $current_tag"
+			image="$(docker images|grep $i|awk 'NR==1 {print $1; exit}'|cut -d "/" -f2)"
+			
+			echo -e "[-] Fetching Tags from docker hub"
+			required_tag="$(wget -q https://registry.hub.docker.com/v1/repositories/dnif/"$image"/tags -O - | tr -d '[]" ' | tr '}' '\n' | awk -F: '{print $3}'|sort -V|awk 'END{print}' )"
+			
 
-container_list=("core" "datanode" "adapter" "console" )
-echo -e "[-] Finding docker Image"
-for i in "${container_list[@]}"
-do
-	#echo -e "[-] Finding docker Image"
-	if [ "$(docker images|grep $i|awk 'NR==1 {print $1; exit}'|cut -d "/" -f2)" ]; then
-		#echo -e "[-] Found $i docker container"
-		echo -e "[-] Checking for current running version"
-		sleep 3
-		current_tag="$(docker images|grep $i|awk 'NR==1 {print $2; exit}')"
-		
-		echo -e "[-] Found current version $current_tag"
-		image="$(docker images|grep $i|awk 'NR==1 {print $1; exit}'|cut -d "/" -f2)"
-		
-		echo -e "[-] Fetching Tags from docker hub"
-		required_tag="$(wget -q https://registry.hub.docker.com/v1/repositories/dnif/"$image"/tags -O - | tr -d '[]" ' | tr '}' '\n' | awk -F: '{print $3}'|sort -V|awk 'END{print}' )"
-		
-
-		if [ "$(printf '%s\n' "$required_tag" "$current_tag" | sort -V | head -n1)" != "$required_tag" ]; then
-		
-			upgrade_docker_container $i $current_tag $required_tag
-		else
-			echo -e "up-to-date ${required_tag}\n"
+			if [ "$(printf '%s\n' "$required_tag" "$current_tag" | sort -V | head -n1)" != "$required_tag" ]; then
+			
+				upgrade_docker_container $i $current_tag $required_tag
+			else
+				echo -e "up-to-date ${required_tag}\n"
+			fi
 		fi
-	fi
-done
-
+	done
+fi
 
 
