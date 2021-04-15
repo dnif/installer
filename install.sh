95eccf3a3ce10595f4a97442f9b2ebbb528cea32
Nikhil Salgaonkar <nsalgaocar@gmail.com>
2021-04-16 00:57:30 +0530

    proxy changes for 9.0.2

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 set -e
 
 
+
+
+
 function compose_check() {
 	if [ -x "$(command -v docker-compose)" ]; then
 		version=$(docker-compose --version |cut -d ' ' -f3 | cut -d ',' -f1)
@@ -94,94 +97,123 @@ function sysctl_check() {
 
 }
 
+function set_proxy() {
+
+	echo "HTTP_PROXY="\"$ProxyUrl"\"" >> /etc/environment
+	echo "HTTPS_PROXY="\"$ProxyUrl"\"" >> /etc/environment
+	echo "https_proxy="\"$ProxyUrl"\"" >> /etc/environment
+	echo "http_proxy="\"$ProxyUrl"\"" >> /etc/environment
+	
+	export HTTP_PROXY=$ProxyUrl 
+	export HTTPS_PROXY=$ProxyUrl 
+	export https_proxy=$ProxyUrl 
+	export http_proxy=$ProxyUrl
+
+}
 
 
+if [[ $EUID -ne 0 ]]; then
+    echo -e "This script must be run as root ... \e[1;31m[ERROR] \e[0m\n"
+    exit 1
+else
 
+	ARCH=$(uname -m)
+	VER=$(lsb_release -rs)
+	tag="v9.0.2" 		# replace tag by the number of release you want
+	release=$(lsb_release -ds)
+	mkdir -p /DNIF
+	echo -e "\nDNIF Installer for $tag\n"
+	echo -e "for more information and code visit https://github.com/dnif/installer\n"
 
+	echo -e "++ Checking operating system for compatibility...\n"
 
-ARCH=$(uname -m)
-VER=$(lsb_release -rs)
-tag="v9.0.2" 		# replace tag by the number of release you want
-release=$(lsb_release -ds)
-mkdir -p /DNIF
-echo -e "\nDNIF Installer for $tag\n"
-echo -e "for more information and code visit https://github.com/dnif/installer\n"
-
-echo -e "++ Checking operating system for compatibility...\n"
-
-echo -n "Operating system compatibility"
-sleep 2
-if [[ "$VER" = "20.04" ]] && [[ "$ARCH" = "x86_64" ]];  then # replace 20.04 by the number of release you want
-	echo -e " ... \e[1;32m[OK] \e[0m"
-	echo -n "Architecture compatibility "
-	echo -e " ... \e[1;32m[OK] \e[0m\n"
-	#echo -e "** found  Ubuntu 20.04 (LTS) x86_64\n"
-	echo -e "** found $release $ARCH\n"
-	echo -e "[-] Checking operating system for compatibility - ... \e[1;32m[DONE] \e[0m\n"
-	echo -e "** Please report issues to https://github.com/dnif/installer/issues"
-	echo -e "** for more information visit https://docs.dnif.it/v9/docs/high-level-dnif-architecture\n"
-	echo -e "* Select a DNIF component you would like to install"
-	echo -e "    [1] Core (CO)"
-	echo -e "    [2] Console (LC)"
-	echo -e "    [3] Datanode (DN)"
-       	echo -e "    [4] Adapter (AD)\n"
-	#read -p "Pick the number corresponding to the component (1 - 4): " COMP
-	COMP=""
-	while [[ ! $COMP =~ ^[1-4] ]]; do
-		echo -e "Pick the number corresponding to the component (1 - 4):  \c"
-                read -r COMP
-        done
-	echo -e "-----------------------------------------------------------------------------------------"
-	case "${COMP^^}" in
-		1)
-			echo -e "[-] Installing the CORE \n"
-			sleep 2
-			docker_check
-			compose_check
-			sysctl_check
-			echo -e "[-] Checking for JDK \n"
-			if type -p java; then
-				_java=java
-			elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-				echo -e "[-] Found java executable in $JAVA_HOME \n"
-				_java="$JAVA_HOME/bin/java"
-			else
-				default="Y"
-				echo -e "[-] To proceed further you have to install openjdk14 before installation\n"
-				read -p "[-] To install OpenJdk14 type [Y/n] " var
-				#read -r var
-				input=${var:-$default}
-				temp=${input^^}
-				if [ "$temp" == "Y" ]; then
-					apt-get -y install openjdk-14-jdk&>> /DNIF/install.log
-				else
-					echo "[-] Aborted"
-					exit 0
-				fi
-			fi
-			if [[ "$_java" ]]; then
-				version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-				if [[ "$version" == "14.0.2" ]]; then
-					echo -e "[-] OpenJdk $version version is running\n"
-				fi
-			fi
-			echo -e "\n[-] Pulling docker Image for CORE\n"
-			docker pull dnif/core:$tag		
-			echo -e "[-] Pulling docker Image for Datanode\n"
-			docker pull dnif/datanode:$tag    	
-			cd /
-			sudo mkdir -p DNIF
-			COREIP=""
-			while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
-				echo -e "ENTER CORE IP: \c"
-				read -r COREIP
+	echo -n "Operating system compatibility"
+	sleep 2
+	if [[ "$VER" = "20.04" ]] && [[ "$ARCH" = "x86_64" ]];  then # replace 20.04 by the number of release you want
+		echo -e " ... \e[1;32m[OK] \e[0m"
+		echo -n "Architecture compatibility "
+		echo -e " ... \e[1;32m[OK] \e[0m\n"
+		echo -e "** found $release $ARCH\n"
+		echo -e "[-] Checking operating system for compatibility - ... \e[1;32m[DONE] \e[0m\n"
+		echo -e "** Please report issues to https://github.com/dnif/installer/issues"
+		echo -e "** for more information visit https://docs.dnif.it/v9/docs/high-level-dnif-architecture\n"
+		echo -e "* Select a DNIF component you would like to install"
+		echo -e "    [1] Core (CO)"
+		echo -e "    [2] Console (LC)"
+		echo -e "    [3] Datanode (DN)"
+			echo -e "    [4] Adapter (AD)\n"
+		COMP=""
+		while [[ ! $COMP =~ ^[1-4] ]]; do
+			echo -e "Pick the number corresponding to the component (1 - 4):  \c"
+					read -r COMP
 			done
-			#ProxyUrl=""
-			#while [[ ! "$ProxyUrl" ]]; do
-			#	echo -e "ENTER Proxy url: \c"
-			#	read -r ProxyUrl
-			#done
-			sudo echo -e "version: "\'2.0\'"
+		echo -e "-----------------------------------------------------------------------------------------"
+		case "${COMP^^}" in
+			1)
+				echo -e "[-] Installing the CORE \n"
+				sleep 2
+				if [[ "$1" == "proxy" ]]; then
+					ProxyUrl=""
+					while [[ ! "$ProxyUrl" ]]; do
+						echo -e "ENTER Proxy url: \c"
+						read -r ProxyUrl
+					done
+					set_proxy $ProxyUrl
+				fi
+				docker_check
+				compose_check
+				sysctl_check
+				if [[ $ProxyUrl ]]; then
+					mkdir -p /etc/systemd/system/docker.service.d
+					echo -e "[Service]
+	Environment=\"HTTPS_PROXY=$ProxyUrl\"">/etc/systemd/system/docker.service.d/http-proxy.conf
+				
+					sudo systemctl daemon-reload
+					sudo systemctl restart docker
+				fi
+				echo -e "[-] Checking for JDK \n"
+				if type -p java; then
+					_java=java
+				elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
+					echo -e "[-] Found java executable in $JAVA_HOME \n"
+					_java="$JAVA_HOME/bin/java"
+				else
+					default="Y"
+					echo -e "[-] To proceed further you have to install openjdk14 before installation\n"
+					read -p "[-] To install OpenJdk14 type [Y/n] " var
+					#read -r var
+					input=${var:-$default}
+					temp=${input^^}
+					if [ "$temp" == "Y" ]; then
+						apt-get -y install openjdk-14-jdk&>> /DNIF/install.log
+					else
+						echo "[-] Aborted"
+						exit 0
+					fi
+				fi
+				if [[ "$_java" ]]; then
+					version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+					if [[ "$version" == "14.0.2" ]]; then
+						echo -e "[-] OpenJdk $version version is running\n"
+					fi
+				fi
+				echo -e "\n[-] Pulling docker Image for CORE\n"
+				docker pull dnif/core:$tag		
+				echo -e "[-] Pulling docker Image for Datanode\n"
+				docker pull dnif/datanode:$tag    	
+				cd /
+				sudo mkdir -p DNIF
+				COREIP=""
+				while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
+					echo -e "ENTER CORE IP: \c"
+					read -r COREIP
+				done
+				#ProxyUrl=""
+				#while [[ ! "$ProxyUrl" ]]; do
+				#	echo -e "ENTER Proxy url: \c"
+				#	read -r ProxyUrl
+				#done
+				sudo echo -e "version: "\'2.0\'"
 services:
   core:
     image: dnif/core:$tag
@@ -195,6 +227,7 @@ services:
       - /DNIF/backup/core:/backup
     environment:
       - "\'CORE_IP="$COREIP"\'"
+      - "\'PROXY="$ProxyUrl"\'"
     ulimits:
       memlock:
         soft: -1
@@ -221,26 +254,26 @@ services:
         soft: -1
         hard: -1
     container_name: datanode-master-v9">>/DNIF/docker-compose.yaml
-			cd /DNIF || exit
-			echo -e "[-] Starting container... \n"
-			docker-compose up -d
-			echo -e "[-] Starting container... \e[1;32m[DONE] \e[0m\n"
-			docker ps
-			echo -e "** Congratulations you have successfully installed the CORE \n"
-			;;
-	
-		2)
-			echo -e "[-] Installing the Console \n"
-			sleep 5
-			docker_check
-			compose_check
-			sysctl_check
-			echo -e "[-] Pulling docker Image for Console\n"
-			docker pull dnif/console:$tag   	
-			cd /
-			sudo mkdir -p /DNIF
-			sudo mkdir -p /DNIF/LC
-			sudo echo -e "version: "\'2.0\'"
+				cd /DNIF || exit
+				echo -e "[-] Starting container... \n"
+				docker-compose up -d
+				echo -e "[-] Starting container... \e[1;32m[DONE] \e[0m\n"
+				docker ps
+				echo -e "** Congratulations you have successfully installed the CORE \n"
+				;;
+		
+			2)
+				echo -e "[-] Installing the Console \n"
+				sleep 5
+				docker_check
+				compose_check
+				sysctl_check
+				echo -e "[-] Pulling docker Image for Console\n"
+				docker pull dnif/console:$tag   	
+				cd /
+				sudo mkdir -p /DNIF
+				sudo mkdir -p /DNIF/LC
+				sudo echo -e "version: "\'2.0\'"
 services:
  console:
   image: dnif/console:$tag
@@ -251,56 +284,74 @@ services:
   volumes:
    - /DNIF/LC:/dnif/lc
   container_name: console-v9">/DNIF/LC/docker-compose.yaml
-  			cd /DNIF/LC || exit
-			echo -e "[-] Starting container... \n"
-			docker-compose up -d
-			echo -e "[-] Starting container... DONE\n"
-			docker ps
-			echo -e "** Congratulations you have successfully installed the Console\n"
-			;;
-		3)
-			echo -e "[-] Installing the Datanode\n"
-			sleep 5
-			docker_check
-			compose_check
-			sysctl_check
-			echo -e "[-] Checking for JDK \n"
-			if type -p java; then
-				_java=java
-			elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-				echo -e "[-] Found java executable in $JAVA_HOME \n"
-				_java="$JAVA_HOME/bin/java"
-			else
-				default="Y"
-				echo -e "[-] To proceed further you have to  install openjdk14 before installation\n"
-				read -p "[-] To install OpenJdk14 type [Y/n] " var
-				#read -r var
-				input=${var:-$default}
-				temp=${input^^}
-				if [ "$temp" == "Y" ]; then
-					apt-get -y install openjdk-14-jdk&>> /DNIF/install.log
+				cd /DNIF/LC || exit
+				echo -e "[-] Starting container... \n"
+				docker-compose up -d
+				echo -e "[-] Starting container... DONE\n"
+				docker ps
+				echo -e "** Congratulations you have successfully installed the Console\n"
+				;;
+			3)
+				echo -e "[-] Installing the Datanode\n"
+
+				sleep 5
+				if [[ "$1" == "proxy" ]]; then
+					ProxyUrl=""
+					while [[ ! "$ProxyUrl" ]]; do
+						echo -e "ENTER Proxy url: \c"
+						read -r ProxyUrl
+					done
+					set_proxy $ProxyUrl
+				fi
+				docker_check
+				compose_check
+				sysctl_check
+				if [[ $ProxyUrl ]]; then
+					mkdir -p /etc/systemd/system/docker.service.d
+					echo -e "[Service]
+	Environment=\"HTTPS_PROXY=$ProxyUrl\"">/etc/systemd/system/docker.service.d/http-proxy.conf
+				
+					sudo systemctl daemon-reload
+					sudo systemctl restart docker
+				fi
+
+				echo -e "[-] Checking for JDK \n"
+				if type -p java; then
+					_java=java
+				elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
+					echo -e "[-] Found java executable in $JAVA_HOME \n"
+					_java="$JAVA_HOME/bin/java"
 				else
-					echo "[-] Aborted"
-					exit 0
+					default="Y"
+					echo -e "[-] To proceed further you have to  install openjdk14 before installation\n"
+					read -p "[-] To install OpenJdk14 type [Y/n] " var
+					#read -r var
+					input=${var:-$default}
+					temp=${input^^}
+					if [ "$temp" == "Y" ]; then
+						apt-get -y install openjdk-14-jdk&>> /DNIF/install.log
+					else
+						echo "[-] Aborted"
+						exit 0
+					fi
 				fi
-			fi
-			if [[ "$_java" ]]; then
-				version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-				if [[ "$version" == "14.0.2" ]]; then
-					echo -e "[-] OpenJdk $version version is running\n"
+				if [[ "$_java" ]]; then
+					version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+					if [[ "$version" == "14.0.2" ]]; then
+						echo -e "[-] OpenJdk $version version is running\n"
+					fi
 				fi
-			fi
-			sleep 5
-			echo -e "\n[-] Pulling docker Image for Datanode\n"
-			docker pull dnif/datanode:$tag		
-			COREIP=""
-			while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
-				echo -e "ENTER CORE IP: \c"
-				read -r COREIP
-			done
-			sudo mkdir -p /DNIF
-			sudo mkdir -p /DNIF/DL
-			sudo echo -e "version: "\'2.0\'"
+				sleep 5
+				echo -e "\n[-] Pulling docker Image for Datanode\n"
+				docker pull dnif/datanode:$tag		
+				COREIP=""
+				while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
+					echo -e "ENTER CORE IP: \c"
+					read -r COREIP
+				done
+				sudo mkdir -p /DNIF
+				sudo mkdir -p /DNIF/DL
+				sudo echo -e "version: "\'2.0\'"
 services:
   datanode:
     privileged: true
@@ -323,32 +374,48 @@ services:
         soft: -1
         hard: -1
     container_name: datanode-v9">>/DNIF/DL/docker-compose.yaml
-    			cd /DNIF/DL || exit
-			IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-			echo -e "[-] Starting container... \n"
-			docker-compose up -d
-			echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m"
-			docker ps
-			echo -e "** Congratulations you have successfully installed the Datanode\n"
-			echo -e "**   Activate the Datanode ($IP) from the components page\n"
-			;;
-		4)
-			echo -e "[-] Installing the ADAPTER \n"
-			sleep 5
-			docker_check
-			compose_check
-			sysctl_check
-			echo -e "[-] Pulling docker Image for Adapter\n"
-			docker pull dnif/adapter:$tag 		
-			COREIP=""
-			while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
-				echo -e "ENTER CORE IP: \c"
-				read -r COREIP
-			done
-			cd /
-			sudo mkdir -p /DNIF
-			sudo mkdir -p /DNIF/AD
-			sudo echo -e "version: "\'2.0\'"
+				cd /DNIF/DL || exit
+				IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+				echo -e "[-] Starting container... \n"
+				docker-compose up -d
+				echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m"
+				docker ps
+				echo -e "** Congratulations you have successfully installed the Datanode\n"
+				echo -e "**   Activate the Datanode ($IP) from the components page\n"
+				;;
+			4)
+				echo -e "[-] Installing the ADAPTER \n"
+				if [[ "$1" == "proxy" ]]; then
+					ProxyUrl=""
+					while [[ ! "$ProxyUrl" ]]; do
+						echo -e "ENTER Proxy url: \c"
+						read -r ProxyUrl
+					done
+					set_proxy $ProxyUrl
+				fi
+				docker_check
+				compose_check
+				sysctl_check
+				if [[ $ProxyUrl ]]; then
+					mkdir -p /etc/systemd/system/docker.service.d
+					echo -e "[Service]
+	Environment=\"HTTPS_PROXY=$ProxyUrl\"">/etc/systemd/system/docker.service.d/http-proxy.conf
+				
+					sudo systemctl daemon-reload
+					sudo systemctl restart docker
+				fi
+
+				echo -e "[-] Pulling docker Image for Adapter\n"
+				docker pull dnif/adapter:$tag 		
+				COREIP=""
+				while [[ ! $COREIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
+					echo -e "ENTER CORE IP: \c"
+					read -r COREIP
+				done
+				cd /
+				sudo mkdir -p /DNIF
+				sudo mkdir -p /DNIF/AD
+				sudo echo -e "version: "\'2.0\'"
 services:
  adapter:
   image: dnif/adapter:$tag
@@ -358,21 +425,23 @@ services:
    - NET_ADMIN
   environment:
    - "\'CORE_IP="$COREIP"\'"
+   - "\'PROXY="$ProxyUrl"\'"
   volumes:
    - /DNIF/AD:/dnif
    - /DNIF/backup/ad:/backup
   container_name: adapter-v9">/DNIF/AD/docker-compose.yaml
-  			cd /DNIF/AD || exit
-			IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-			echo -e "[-] Starting container...\n "
-			docker-compose up -d
-			echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m\n"
-			docker ps
-			echo -e "** Congratulations you have successfully installed the Adapter\n"
-			echo -e "**   Activate the Adapter ($IP) from the components page\n"
-			;;
-		esac
+				cd /DNIF/AD || exit
+				IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+				echo -e "[-] Starting container...\n "
+				docker-compose up -d
+				echo -e "[-] Starting container ... \e[1;32m[DONE] \e[0m\n"
+				docker ps
+				echo -e "** Congratulations you have successfully installed the Adapter\n"
+				echo -e "**   Activate the Adapter ($IP) from the components page\n"
+				;;
+			esac
 
-else
-	echo -e "\e[0;31m[ERROR] \e[0m Operating system is incompatible"
+	else
+		echo -e "\e[0;31m[ERROR] \e[0m Operating system is incompatible"
+	fi
 fi
